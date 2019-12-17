173998a9d61c3f3428f4534c518af55f4876686d
AzatOmarov <kst369@mail.ru>
2019-12-17 22:37:08 +0100

    Import main components into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './container/home/Home';
+import About from './container/about/About';
+import Stories from './container/stories/Stories';
+import Navigation from './container/Navigation';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h2>Hello</h2>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navigation />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/stories" component={Stories} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
