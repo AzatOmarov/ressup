a23d7500e4a0e5d26ea8e32d1547b36b670e0f61
AzatOmarov <kst369@mail.ru>
2019-12-25 21:18:23 +0100

    Create staticLinks.jsx

---
+++ src/utils/staticLinks.js
@@ -0,0 +1,8 @@
+const fityLinks = {
+  facebook: 'https://athenium2-static-01.s3.eu-central-1.amazonaws.com/1df66fb0-3028-4acc-aa37-85b07e2ae1bb',
+  instagram: 'https://athenium2-static-01.s3.eu-central-1.amazonaws.com/4608badf-c5de-4f47-bf56-9e93b1c37bc9',
+  twitter: 'https://athenium2-static-01.s3.eu-central-1.amazonaws.com/f06684d5-702e-49a4-8998-760f2f3f622c',
+  youtube: 'https://athenium2-static-01.s3.eu-central-1.amazonaws.com/cbc18799-d5ef-4fbb-8a51-9712d19b16c3',
+};
+
+export default fityLinks;
