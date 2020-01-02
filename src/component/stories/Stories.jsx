fa97009f3c4a9e93f52708d3eed300a6d79aebc5
AzatOmarov <kst369@mail.ru>
2020-01-02 22:09:26 +0100

    Create Stories component to render a list of stories

---
+++ src/component/stories/Stories.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { objectOf, object } from 'prop-types';
+import { Link } from 'react-router-dom';
+
+
+const Stories = ({ stories }) => (
+
+  <div className="story-list">
+    {stories.map((story) => (
+      <div className="stories">
+        <Link key={story.id} to={`/stories/${story.id}`}>
+          <h5 className="stories__title">{story.title}</h5>
+        </Link>
+        <p className="stories__text">
+          {story.text.substring(0, 150)}
+        ...
+        </p>
+      </div>
+    ))}
+  </div>
+);
+
+Stories.propTypes = {
+  stories: objectOf(object).isRequired,
+};
+export default Stories;
