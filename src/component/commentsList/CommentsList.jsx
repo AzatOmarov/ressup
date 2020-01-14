4e1556369d9d0dddf1cdba94ceb89028c0ef30f5
AzatOmarov <kst369@mail.ru>
2020-01-14 16:19:53 +0100

    Change CommentsList.jsx

--- src/component/commentsList/CommentsList.jsx
+++ src/component/commentsList/CommentsList.jsx
@@ -3,8 +3,7 @@ import { objectOf, object } from 'prop-types';
 
 const CommentsList = ({ comments }) => (
   <>
-    <h3>Comments:</h3>
-;
+    <h3 style={{ color: 'black' }}>Comments:</h3>
     { comments.map((comment, key) => (
       <div className="comment" key={key}>
         <h4>{comment.userName}</h4>
