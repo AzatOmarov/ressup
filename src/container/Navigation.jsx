1ec2da828f3290f6205c6806f7e3d3beddb95af7
AzatOmarov <kst369@mail.ru>
2020-01-15 08:36:36 +0100

    Clean up the code Navigation.jsx and change CSS

--- src/container/Navigation.jsx
+++ src/container/Navigation.jsx
@@ -9,14 +9,14 @@ const Navigation = ({ children }) => (
   <div className="container">
     <div className="navigation">
       <img src={logo} alt="logo" className="navigation__logo" />
-      <Navbar className="navbar-light bg-light">
-        <div className="navigation-links" style={{ width: '-webkit-fill-available' }}>
-          <Link to="/about" className="navigation-links__link-item">О Нас</Link>
-          <Link to="/story-list" className="navigation-links__link-item">Наши Проекты</Link>
+      <Navbar>
+        <div className="navigation-links">
+          <Link to="/about" className="navigation-links__link-item">О нас</Link>
+          <Link to="/story-list" className="navigation-links__link-item">Наши проекты</Link>
           <Link to="/story-list" className="navigation-links__link-item">Истории</Link>
           <Link to="/story-list" className="navigation-links__link-item">Опросник</Link>
           <Link to="/story-list" className="navigation-links__link-item">Контакты</Link>
-          <Link to="/" className="navigation-links__link-item" />
+          <Link to="/" />
         </div>
       </Navbar>
 
