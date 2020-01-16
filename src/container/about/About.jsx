8cf02268f2190f3406c0e50062f243d5928e025c
AzatOmarov <kst369@mail.ru>
2020-01-16 07:16:14 +0100

    Render Paragraph, Timeline and  TeamMember components within About.jsx

--- src/container/about/About.jsx
+++ src/container/about/About.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Timeline from '../time-line/TimeLine';
+import data from '../../staticData/aboutUs';
+import teamMembers from '../../staticData/teamMembers';
+import Paragraph from '../../component/about-us-paragraph/index';
+import TeamMember from '../../component/team-member/TeamMember';
 
 
 function About() {
+  const team = teamMembers.map((member, index) => (
+    <TeamMember member={member} />
+  ));
   return (
     <>
       <div className="about">
-        <h4>About</h4>
+        <section>
+          <Paragraph data={data.ressup} />
+          <Timeline />
+          <Paragraph data={data.team} />
+        </section>
+        <section className="team-members">
+          {team}
+        </section>
       </div>
     </>
   );
