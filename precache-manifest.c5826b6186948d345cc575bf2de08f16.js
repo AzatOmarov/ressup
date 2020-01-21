a97076bcac187c38ae83a77f27762babd3b2f97e
AzatOmarov <kst369@mail.ru>
2020-01-21 17:41:46 +0100

    Updates

---
+++ precache-manifest.c5826b6186948d345cc575bf2de08f16.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d55d2f05a4dbf3a4bf41048b23f22a65",
+    "url": "/ressup/index.html"
+  },
+  {
+    "revision": "dc36141ea11378549930",
+    "url": "/ressup/static/css/main.358995f0.chunk.css"
+  },
+  {
+    "revision": "cff3369d187b1715f3d2",
+    "url": "/ressup/static/js/2.ff359212.chunk.js"
+  },
+  {
+    "revision": "b1953906fcf37f5c263e0254ff985bef",
+    "url": "/ressup/static/js/2.ff359212.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc36141ea11378549930",
+    "url": "/ressup/static/js/main.dcd18663.chunk.js"
+  },
+  {
+    "revision": "9c8a39ddb1612a6ad31e",
+    "url": "/ressup/static/js/runtime-main.3d5fb9ad.js"
+  },
+  {
+    "revision": "3d7c9946a21fd9bc68a4aeef56c5a037",
+    "url": "/ressup/static/media/aima.3d7c9946.svg"
+  },
+  {
+    "revision": "2729628b0ae56bf4ff1fac0034e2cc9a",
+    "url": "/ressup/static/media/azat.2729628b.svg"
+  },
+  {
+    "revision": "0e6d18b1b11ad9759da34df08eff8458",
+    "url": "/ressup/static/media/deineMeinung.0e6d18b1.svg"
+  },
+  {
+    "revision": "cfa2a0abd902ea8d8efc2b545364a11d",
+    "url": "/ressup/static/media/elvina.cfa2a0ab.svg"
+  },
+  {
+    "revision": "b99c8ee8864d5115a5b89e0aec0044ba",
+    "url": "/ressup/static/media/irene.b99c8ee8.svg"
+  },
+  {
+    "revision": "ad0fb68f90527436a988e4e715de9115",
+    "url": "/ressup/static/media/logo.ad0fb68f.svg"
+  },
+  {
+    "revision": "ebadb21078eddcdd84245143b26f446e",
+    "url": "/ressup/static/media/madina.ebadb210.svg"
+  },
+  {
+    "revision": "bc4c06b390352ab34948c84166f5e368",
+    "url": "/ressup/static/media/marcel.bc4c06b3.svg"
+  },
+  {
+    "revision": "9e769a8c51219299c4c1c318e5348b7c",
+    "url": "/ressup/static/media/nurgul.9e769a8c.svg"
+  },
+  {
+    "revision": "ed6be45c73fde253231465846c2041bb",
+    "url": "/ressup/static/media/retro.ed6be45c.svg"
+  },
+  {
+    "revision": "ac2c7fe1fc0476d9e2132f15d55870fb",
+    "url": "/ressup/static/media/saltanat.ac2c7fe1.svg"
+  }
+]);
