3c08d3b1bbaf9fd0a5f50c03290ba0fcfaf08c0c
AzatOmarov <kst369@mail.ru>
2020-02-23 23:16:17 +0600

    Change UI and CSS of Join.jsx

--- src/component/join/Join.jsx
+++ src/component/join/Join.jsx
@@ -3,35 +3,46 @@ import data from '../../staticData/join';
 
 function Join() {
   const {
-    title, description, whatWeNeed, tellYourStory, address, weLookingFor, messageUs, email,
+    title, description, cooperation, weLookingFor, messageUs,
+    important,
   } = data;
-  const importants = whatWeNeed.importants.map((item, id) => <li><span>{item}</span></li>);
-  const people = weLookingFor.people.map((item, id) => <li><span>{item}</span></li>);
-  const cooperation = <a href="mailto:cooperation@ressup.org">cooperation@ressup.org</a>;
-  const at = <a href="mailto:story@ressup.org">story@ressup.org</a>;
+  const importants = important.items.map((item, id) => <li className="people"><span>{item}</span></li>);
+  const people = weLookingFor.people.map((item, id) => <li className="people"><span>{item}</span></li>);
+  const email = <a href="mailto:cooperation@ressup.org">cooperation@ressup.org</a>;
   return (
     <>
       <div className="join">
         <h4>{title}</h4>
         <p>{description}</p>
-        <p>{whatWeNeed.title}</p>
-        <ul>
-          {importants}
-        </ul>
-        <p>
-          {tellYourStory}
-          {at}
-          {address}
-        </p>
-        <p>{weLookingFor.title}</p>
+        <p className="weLookingFor-title">{weLookingFor.title}</p>
         <ul>
           {people}
         </ul>
-        <p>{weLookingFor.messageUs}</p>
-        <p>
+        <p className="weLookingFor-message-us">{weLookingFor.messageUs}</p>
+        <p className="important-title">{important.title}</p>
+        <ul>
+          {importants}
+        </ul>
+        <p className="message-us">
           {messageUs}
-          {cooperation}
         </p>
+        <div className="cooperation">
+          <p className="cooperation-title">
+            {cooperation.title}
+          </p>
+          <p className="cooperation-item">
+            {cooperation.messageUs}
+            {' '}
+            {email}
+          </p>
+          <p className="cooperation-item">
+            { cooperation.financialSupport }
+          </p>
+          <p className="bank-details">{cooperation.financialData[0]}</p>
+          <p className="bank-details">{cooperation.financialData[1]}</p>
+          <p className="bank-details">{cooperation.financialData[2]}</p>
+        </div>
+
       </div>
     </>
   );
