dd47ba4dfd202b277ed76836268c6960b49e8351
AzatOmarov <kst369@mail.ru>
2020-02-23 18:11:52 +0600

    Change project's paragraph component

--- src/component/project/paragraph/index.jsx
+++ src/component/project/paragraph/index.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { string, func } from 'prop-types';
 import ogorod from '../../../assets/img/ogorod.svg';
 
 
-const Paragraph = ({ title, description }) => (
-  <>
-    <img src={ogorod} alt="ogorod" />
-    <h4 className="title">{title}</h4>
-    <hr />
-    <p>{description}</p>
-    <div className="nextButton">
-      <span className="nextButton__text">
-            далее
-      </span>
+function Paragraph(props) {
+  const {
+    title, description, index, onClick,
+  } = props;
+  return (
+    <div className="project-item">
+      <img src={ogorod} alt="ogorod" className="project-item__image" />
+      <h4 className="project-item__title">{title}</h4>
+      <hr />
+      <div className="project-item__text-and-button">
+        <div className="description">{description}</div>
+        <div className="nextButton" onClick={onClick}>
+          <Link to={`/procs/${index}`} className="fill" />
+          <p>далее</p>
+        </div>
+      </div>
     </div>
-  </>
-);
+  );
+}
+
+Paragraph.propTypes = {
+  title: string.isRequired,
+  description: string.isRequired,
+  index: string.isRequired,
+  onClick: func.isRequired,
+};
 
 export default Paragraph;
