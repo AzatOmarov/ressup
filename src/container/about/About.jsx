d24e21ae4c97408bd4758f2b0e477405b278c47c
AzatOmarov <kst369@mail.ru>
2020-03-29 13:16:42 +0600

    REF: restructure about page

--- src/container/about/About.jsx
+++ src/container/about/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Timeline from '../time-line/TimeLine';
 import data from '../../staticData/aboutUs';
 import teamMembers from '../../staticData/teamMembers';
@@ -6,13 +7,16 @@ import Paragraph from '../../component/about-us-paragraph/index';
 import TeamMember from '../../component/team-member/TeamMember';
 import Footer from '../footer/Footer';
 
+
 const style = {
   bottom: '-10%',
 };
 
 function About() {
-  const team = teamMembers.map((member, index) => (
-    <TeamMember member={member} />
+  const team = teamMembers.map((member) => (
+    <div key={member.id}>
+      <TeamMember member={member} />
+    </div>
   ));
   const { title, description } = data.ressup;
   const { title: teamTitle, text: teamDescription } = data.team;
@@ -22,20 +26,18 @@ function About() {
         <Paragraph title={title} description={description} />
         <Timeline />
         <Paragraph title={teamTitle} description={teamDescription} />
+        <div className="about__link">
+          <div className="interested">Если вам интересно узнать о предыдущих социальных проектах с нашим участием , то вы можете почитать о них тут.</div>
+          <div className="nextButton">
+            <Link to="https://www.youtube.com/channel/UCv-LYTGGNAytZRuGbJhPeRA" className="fill">
+            далее
+            </Link>
+          </div>
+        </div>
         <section className="team-members">
           {team}
         </section>
-        <p id="about__link">
-          Eсли Вам интересно узнать о нас больше, нажмите на данную
-          {' '}
-          <a
-            href="https://www.youtube.com/channel/UCv-LYTGGNAytZRuGbJhPeRA"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            ссылку
-          </a>
-        </p>
+
       </div>
       <Footer style={style} />
     </>
