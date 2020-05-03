a3e6cf6d93a259d4b13b94c08020de7d5cdd9ca7
AzatOmarov <kst369@mail.ru>
2020-05-03 19:26:30 +0600

    STYLE: Set font-size to 20px, changed UI of login page

---
+++ src/staticData/questionaire.js
@@ -0,0 +1,13 @@
+const data = {
+    title: 'Контакты',
+    howToContactUs: 'Связаться с нами можно, написав на',
+    email: 'contact@ressup.org',
+    or: 'или используя форму ниже',
+    yourEmail: 'Ваш e-mail',
+    yourName: 'Ваше имя',
+    yourMessage: 'Ваш текст письма',
+  };
+  
+  
+  export default data;
+  
