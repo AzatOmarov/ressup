18946b9a08f37147287dcd85e276887dd8a7f8b8
AzatOmarov <kst369@mail.ru>
2020-08-08 17:33:56 +0600

    FEAT: Update about page and make a screen mobile friendly

--- src/container/time-line/TimeLine.jsx
+++ src/container/time-line/TimeLine.jsx
@@ -1,21 +1,10 @@
-// import React from 'react';
-// import timelineData from '../../staticData/timeLineData';
-// import TimelineItem from '../../component/time-line-item/TimeLineItem';
-
-
-// const Timeline = () => timelineData.length > 0 && (
-// <div className="timeline-container2">
-//   {timelineData.map((data, idx) => (
-//     <TimelineItem data={data} key={idx} />
-//   ))}
-// </div>
-// );
-
-// export default Timeline;
 import React from 'react';
 import { Timeline } from 'react-material-timeline';
-import { Avatar, Icon } from '@material-ui/core';
-import timelineData from '../../staticData/timeLineData';
+import { Avatar, Icon, colors } from '@material-ui/core';
+import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import PeopleIcon from '@material-ui/icons/People';
+import GolfCourseIcon from '@material-ui/icons/GolfCourse';
 
 // const icon =
 
@@ -34,16 +23,51 @@ import timelineData from '../../staticData/timeLineData';
 //   }
 // ];
 
+const theme = createMuiTheme({
+  overrides: {
+    MuiPaper: {
+      root: {
+        background: 'inherit !important',
+        boxShadow: 'none !important'
+      },
+    },
+    MuiAvatar: {
+      root: {
+        // width: '10px !important',
+        // height: '10px !important',
+        backgroundColor: '#F86C59 !important'
+      }
+    },
+    MuiCardHeader: {
+      root: {
+        padding: '16px 16px 0 16px'
+      }
+    },
+    MuiCardContent: {
+      root: {
+        padding: '0 16px'
+      }
+    }, // #f3f3f3
+    MuiSvgIcon: {
+      root: {
+        color: '#f3f3f3'
+      }
+    }
+
+
+  }
+});
+
 const events = [
   {
     // id:0,
     title: 'Что мы делаем',
     description: [
-      'Мы находим социальные проекты, которые оказывают поддержку в социализации, интеграции и инклюзии социально-уязвимых слоев населения.',
-      'Мы делимся только теми проектами, которые за время их существования доказали свою эффективность, и с которыми мы познакомились лично.',
+      'Мы находим социальные проекты, которые оказывают поддержку в социализации, интеграции и инклюзии социально-уязвимых слоев населения. ',
+      'Мы делимся только теми проектами, которые за время их существования доказали свою эффективность, и с которыми мы познакомились лично. ',
       'Мы помогаем вам связаться с этими  проектами и получить необходимую информацию для их адаптации под ваши условия.',
     ],
-  icon: <Avatar><Icon>{' '}</Icon></Avatar>,
+    icon: <Avatar><BusinessCenterIcon color='inherit' /></Avatar>,
   },
   {
     // id:1,
@@ -52,19 +76,22 @@ const events = [
       'Для некоммерческих организаций.',
       'Для социальных предпринимателей.',
       'Для частных лиц, которые хотели бы запустить свой  собственный социальный проект.'],
-      icon: <Avatar><Icon>{' '}</Icon></Avatar>,
+    icon: <Avatar><PeopleIcon color='inherit' /></Avatar>,
   },
   {
     // id:2,
     title: 'Наша миссия',
     description: ['Вдохновлять людей на инициирование полезных для общества проектов.',
       'Поддерживать новые инициативы в налаживании контакта и сотрудничества с организаторами действующих проектов для обмена опытом.'],
-      icon: <Avatar><Icon>{' '}</Icon></Avatar>,
+    icon: <Avatar> <GolfCourseIcon color='inherit' /> </Avatar>,
   },
 ];
 
 export default class AwesomeTimeline extends React.Component {
   render() {
-    return <Timeline styles={{ rightCard: { textAlign: 'left' } }} events={events} />;
+    return <ThemeProvider theme={theme}>
+      <Timeline styles={{ rightCard: { textAlign: 'left' }, leftCard: { textAlign: 'right', cardDecoratorLeft: 'red' } }} events={events} />
+    </ThemeProvider>
+
   }
 }
